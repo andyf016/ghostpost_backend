1ceb7e92d85215bc4ec55ab7654f83932dd35479
Andrew Fillenwarth <andyf016@gmail.com>
2020-10-01 00:26:49 -0400

    set up router and urls

--- reactpost/urls.py
+++ reactpost/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+from postapp import views
+
+router = routers.DefaultRouter()
+router.register(r'post', views.PostViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
