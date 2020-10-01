38ac730ef88046cb1592571418e4addc46b47c6a
Andrew Fillenwarth <andyf016@gmail.com>
2020-10-01 00:10:53 -0400

    complete model, make migrations

--- postapp/models.py
+++ postapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    CHOICES = (('b', 'Boast'), ('r', 'Roast'))
+    sentiment = models.CharField(max_length=10, choices=CHOICES, default='b')
+    body = models.CharField(max_length=280)
+    up_votes = models.IntegerField(default=0)
+    down_votes = models.IntegerField(default=0)
+    total_votes = models.IntegerField(default=0)
+    created = models.DateTimeField(default=timezone.now)
+    update = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.body
+
