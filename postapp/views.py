f54ec25f2c4d9ca3450bd5127f7385a3e03091c6
Andrew Fillenwarth <andyf016@gmail.com>
2020-10-01 00:24:07 -0400

    set up serializer and viewset

--- postapp/views.py
+++ postapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from rest_framework import viewsets
+from postapp.models import Post
+from postapp.serializers import PostSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
 # Create your views here.
