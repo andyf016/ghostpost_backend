aaf1d35bcc29d629c5ef8a0809725d5f9056e426
Andrew Fillenwarth <andyf016@gmail.com>
2020-10-01 20:37:45 -0400

    setup first action decorator, testing

--- postapp/views.py
+++ postapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from postapp.models import Post
 from postapp.serializers import PostSerializer
 
@@ -8,4 +10,14 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-# Create your views here.
+    @action(detail=False)
+    def list_boasts(self, request):
+        boasts = Post.objects.filter(sentiment='b')
+
+        page = self.paginate_queryset(boasts)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(boasts, many=True)
+        return Response(serializer.data)
