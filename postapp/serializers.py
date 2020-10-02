14af6791945c80b20abb147b9fe4fbfcea96b833
Andrew Fillenwarth <andyf016@gmail.com>
2020-10-02 01:05:52 -0400

    update serializer

--- postapp/serializers.py
+++ postapp/serializers.py
@@ -4,4 +4,12 @@ from postapp.models import Post
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = [
+            'id',
+            'sentiment',
+            'body',
+            'up_votes',
+            'down_votes',
+            'total_votes',
+            'created',
+            'update']
