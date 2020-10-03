09ae657727a1e91ada165a0699e67abad02a7f46
Andrew Fillenwarth <andyf016@gmail.com>
2020-10-02 23:28:38 -0400

    finishing up

--- postapp/serializers.py
+++ postapp/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 from postapp.models import Post
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
-    sentiment_display = serializers.CharField(source='get_sentiment_display')
+    # sentiment_display = serializers.CharField(source='get_sentiment_display')
     class Meta:
         model = Post
         fields = [
             'id',
             'url',
-            'sentiment_display',
+            # 'sentiment_display',
             'sentiment',
             'body',
             'up_votes',
